107d559d31f0c1bd569cc392e8bd3888e2f39bfb
M. Shoaib Anwar <mshoaibanwar2001@gmail.com>
2024-03-07 01:10:41 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import TopNav from './components/TopNav'
+import Product from './components/Product';
+import Orders from './components/Orders';
+import { useState } from 'react';
+import Checkout from './components/Checkout';
+import Reset from './components/Reset';
+import React from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const[pshow, setpShow] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <BrowserRouter>
+          <TopNav setpState={setpShow}/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route index element={<Home />} />
+            <Route path="product" element={<Product />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="reset" element={<Reset />} />
+          </Routes>
+          <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import store from './reduxStore/store';
+
+import { saveState } from "./reduxStore/browserStorage";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="385627185644-14sv7efruhft29hmd5jooboci629m5u4.apps.googleusercontent.com">
+    <Provider store={store}>
+      <App />
+    </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
